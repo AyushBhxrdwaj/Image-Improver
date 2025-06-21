a6107fd55b40d192f63f10505677a0f851467ccd
Ayush Bhardwaj <ayush.ds321@gmail.com>
2025-06-21 19:00:57 +0530

    download button

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Load from './Load'
-
 const Preview = (props) => {
+  const [loaded, setloaded] = useState(false)
   return (
     <div className='mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl'>
       {/* Original Image */}
@@ -16,16 +16,19 @@ const Preview = (props) => {
       {/* Enhanced Image */}
       <div className='bg-gradient-to-tr from-gray-500 via-gray-700 to-gray-900 shadow-lg rounded-xl overflow-hidden'>
         <h2 className='text-xl font-semibold text-center bg-blue-900 text-white py-2 px-3'>Enhanced Image</h2>
+
       
         {props.enhanced && !props.loading && (<img className='w-full h-full object-cover' src={props.enhanced} alt="" />)}
         
         
+
         {props.loading?<Load/>:(
           <div className='flex text-white font-bold items-center justify-center h-80 bg-gradient-to-tr from-gray-500 via-gray-700 to-gray-900'>No enhanced Image</div>
         )}
         
         
       </div>
+      {!props.loading && props.enhanced && (<a href={props.enhanced} download="enhanced.jpg" className='bg-blue-600 rounded-full text-white font-bold px-2 py-3 w-40 hover:bg-violet-600 transition text-center'>Download</a>) }
     </div>
   )
 }
